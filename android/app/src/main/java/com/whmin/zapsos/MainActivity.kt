c27e6cc7bf9740646f0e2a4e912a90381af9b16b
Minaro <38708197+Minar0@users.noreply.github.com>
2023-06-15 21:24:00 -0500

    Now it can pull the secret

--- android/app/src/main/java/com/whmin/zapsos/MainActivity.kt
+++ android/app/src/main/java/com/whmin/zapsos/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.whmin.zapsos
 
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -8,6 +11,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.TextView.OnEditorActionListener
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var inputBox: EditText
     lateinit var responseBox: TextView
+    lateinit var metadata: Bundle
     var userInput: String = ""
     //val IntentRecognizer = IntentRecogManager()
 
@@ -24,8 +29,6 @@ class MainActivity : AppCompatActivity() {
 
         inputBox=findViewById(R.id.userInput)
         responseBox=findViewById(R.id.zapsosResponse)
-
-
 
         inputBox.setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
             //I'm not sure why handled matters
@@ -48,10 +51,27 @@ class MainActivity : AppCompatActivity() {
                 handled = true
             }
             handled
+
         })
+
+        //This is a really fun way to do the metadata initialization using Kotlin's weird if statement variable stuff. metadata is equal to an if statement. if the build version is past a certain amount, it'll set it equal to the true block. If not, it'll use the old way of doing stuff
+        metadata = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+        {
+            this.packageManager.getApplicationInfo(this.packageName,PackageManager.ApplicationInfoFlags.of(0))
+                .metaData
+        }
+        else
+        {
+            this.packageManager
+                .getApplicationInfo(this.packageName, PackageManager.GET_META_DATA)
+                .metaData
+        }
     }
     fun goToSettings(view: View?) {
         val intent = Intent(this, SettingsActivity::class.java)
-        startActivity(intent)
+        val stringy = metadata.getString("SPOTIFY_ID")
+        val toast = Toast.makeText(this, stringy, Toast.LENGTH_LONG) // in Activity
+        toast.show()
+        //startActivity(intent)
     }
 }
