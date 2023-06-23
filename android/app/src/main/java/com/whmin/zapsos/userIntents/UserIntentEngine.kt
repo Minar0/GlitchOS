033d53f035a6bc61e1b4e76cb5a6692c17067a02
Minaro <38708197+Minar0@users.noreply.github.com>
2023-06-23 16:50:10 -0500

    Cleared and reset Intent Engine

--- android/app/src/main/java/com/whmin/zapsos/userIntents/UserIntentEngine.kt
+++ android/app/src/main/java/com/whmin/zapsos/userIntents/UserIntentEngine.kt
@@ -1,37 +1,13 @@
 package com.whmin.zapsos.userIntents
-//
-//import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation
-//import edu.stanford.nlp.pipeline.StanfordCoreNLP
-//import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.BasicDependenciesAnnotation
-//import java.util.*
-//
-//
-//public class IntentRecogManager {
-//    var pipeline: StanfordCoreNLP? = null
-//    fun init() {
-//        val props = Properties()
-//        props.setProperty("annotators", "tokenize, ssplit, pos, parse")
-//        pipeline = StanfordCoreNLP(props)
-//    }
-//
-//    fun findIntent(text: String?) {
-//        val annotation = pipeline!!.process(text)
-//        for (sentence in annotation.get(SentencesAnnotation::class.java)) {
-//            val sg = sentence.get(BasicDependenciesAnnotation::class.java)
-//            var intent = "It does not seem that the sentence expresses an explicit intent."
-//            for (edge in sg.edgeIterable()) {
-//                if (edge.relation.longName === "direct object") {
-//                    val tverb = edge.governor.originalText()
-//                    var dobj = edge.dependent.originalText()
-//                    dobj = dobj.substring(0, 1).uppercase(Locale.getDefault()) + dobj.substring(1)
-//                        .lowercase(
-//                            Locale.getDefault()
-//                        )
-//                    intent = tverb + dobj
-//                }
-//            }
-//            println("Sentence:\t$sentence")
-//            println("Intent:\t\t$intent\n")
-//        }
-//    }
-//}
+
+import java.util.regex.Pattern
+
+
+class UserIntentEngine {
+    fun init() {
+    }
+
+    fun findIntent(text: String?) {
+
+    }
+}
